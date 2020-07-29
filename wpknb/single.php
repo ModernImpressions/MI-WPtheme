ee4196273a8ecf7e58171396a58e1804c59c9d38
Patrick Barnhardt <contact@crimsonstrifegames.com>
2020-07-29 15:24:40 -0400

    fix for "unexpected 'else'

--- wpknb/single.php
+++ wpknb/single.php
@@ -67,8 +67,6 @@ get_header(); ?>
 
                         </div> 
                         <?php endwhile; ?>
-                        <?php else : ?>
-                        <h3><?php _e('404 Error&#58; Not Found', 'alihossain'); ?></h3>
                         <?php endif; ?>
                     </div>
                 </div>
