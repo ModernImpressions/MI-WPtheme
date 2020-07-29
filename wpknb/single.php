a84765d39e5404452b710f00be62745794c447e1
Patrick Barnhardt <contact@crimsonstrifegames.com>
2020-07-29 15:32:19 -0400

    attempt outright replacement of template page.

--- wpknb/single.php
+++ wpknb/single.php
@@ -1,77 +1,40 @@
 <?php
 /**
-*Template Name: Full Width Page
+*Template Name: Full Width Knowledgebase Article Page
 */
 
 get_header(); ?>
-<?php global $wpknb; ?>
-<style>
-.hentry .entry-title,
-.entry .entry-title {
-    display: none;
-}
-</style>
-
-<!-- Content Area
-    ================================================== -->
-<div id="full_page_area">
-    <div class="inner_full_page_thumb">
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="wpknb">
-                    <div class="wpknb-inner">
-                        <?php if(!empty( $wpknb['title'] )):?>
-                        <h1 class="wpknb-title"><?php echo $wpknb['title'] ;?></h1>
-                        <?php endif;?>
-                        <?php
-		if(!isset($wpknb['search_single']) || (isset($wpknb['search_single']) && $wpknb['search_single'] != '1')){
-			echo do_shortcode('[knowledge_base_search]');
-		}
-		
-	while(have_posts()) : the_post();
-	$term_list = get_the_term_list( get_the_ID() , 'knb-category', '<span class="wpknb-terms">in ',',','</span>');
-	?>
-                    </div>
-                </div>
+  
+    <!-- Content Area
+    ================================================== -->  
+        <div id="full_page_area">
+            <?php if (have_posts()) : ?>	
+            <?php while (have_posts()) : the_post(); ?>             
+            <div class="inner_full_page_thumb">
+                <?php echo the_post_thumbnail(); ?>
             </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="original_content_area wpkb">
-                        <article class="wpknb-single">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="original_content_area">
                             <h2 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-                            <div class="wpknb-meta">
-                                <time class="updated" datetime="<?php echo get_the_modified_date('c'); ?>"
-                                    pubdate><?php echo __('Last Updated:', 'wpKnB') . ' '. human_time_diff(get_the_modified_date('U'), current_time('timestamp')) . ' ' . __('ago', 'wpKnB'); ?></time>
-                                <?php echo $term_list;?>
-                            </div>
-                            <div class="wpknb-single-content">
-                                <?php 
-		        if ( has_post_thumbnail() ) { 
-		          the_post_thumbnail();
-		        } 
-		        ?>
-                                <?php the_content(); ?>
-                            </div>
-                            <?php 
-		    if(!isset($wpknb['voting']) || ( isset($wpknb['voting']) && $wpknb['voting'] != '1' ))
-		    	echo do_shortcode('[knowledge_base_vote id="'. get_the_ID() .'"]');
-		    ?>
-                        </article>
-                    </div>
-                    <div id="comments_template">
+                            <?php the_content(); ?>
+                        </div>
+						<div id="comments_template">
 
-                        <?php comments_template(); ?>
+                            <?php comments_template(); ?>
 
+                        </div> 
+                        <?php endwhile; ?>
+                        <?php else : ?>
+                        <h3><?php _e('404 Error&#58; Not Found', 'alihossain'); ?></h3>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
+       
+        
     <!-- Footer Bottom area
-    ================================================== -->
+    ================================================== -->		
     <?php get_footer(); ?>
