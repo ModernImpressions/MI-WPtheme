9bdf78c08d58e73fe37e7e89efab109cdfc0062b
Patrick Barnhardt <contact@crimsonstrifegames.com>
2020-12-23 14:12:55 -0500

    Update page_staff.php
    fixed formatting. tested removal of the scroller

--- page_staff.php
+++ page_staff.php
@@ -27,68 +27,38 @@ get_header(); ?>
                     </div>
                 </div>
                 <div class="row">
-
-<div id="owl-team-item">
-
-<?php query_posts('post_type=team_area&post_status=publish&posts_per_page=-1&paged='. get_query_var('post')); ?>
-
-<?php if(have_posts()) : ?>
-
-<?php while (have_posts()) : the_post(); ?>	
-
-    <div class="item">
-
-        <div class="child_team_area">
-
-            <?php the_post_thumbnail( 'team_area' ); ?>
-
-            <div class="team_social">
-
-                <div class="team_details">
-
-                    <p><?php echo get_post_meta($post->ID, 'team_position', true); ?></p>
-
-                    
-
-                    <?php $variable = get_post_meta($post->ID, 'team_social_list', true); ?>
-
-                    <?php
-
-                    if ( !empty( $variable ) ) {
-
-                    echo '<ul>';
-
-                    foreach( $variable as $any_name ) {
-
-                    echo '<li><a href="'.$any_name['team_social_link'].'"><i class="'.$any_name['team_social_icon'].'"></i></a></li>';
-
-                    }
-
-                    echo '</ul>';
-
-                    }
-
-                    ?>
-
+                    <div id="team-item">
+                        <?php query_posts('post_type=team_area&post_status=publish&posts_per_page=-1&paged='. get_query_var('post')); ?>
+                            <?php if(have_posts()) : ?>
+                        <?php while (have_posts()) : the_post(); ?>	
+                    <div class="item">
+                        <div class="child_team_area">
+                            <?php the_post_thumbnail( 'team_area' ); ?>
+                            <div class="team_social">
+                                <div class="team_details">
+                                    <p>
+                                        <?php echo get_post_meta($post->ID, 'team_position', true); ?>
+                                    </p>
+                                    <?php $variable = get_post_meta($post->ID, 'team_social_list', true); ?>
+                                    <?php
+                                    if ( !empty( $variable ) ) {
+                                        echo '<ul>';
+                                        foreach( $variable as $any_name ) {
+                                            echo '<li><a href="'.$any_name['team_social_link'].'"><i class="'.$any_name['team_social_icon'].'"></i></a></li>';
+                                        }
+                                        echo '</ul>';
+                                    }?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>   
+                    <?php endwhile; ?>    
+                    <?php endif; ?> 
                 </div>
+                </div>
+                </div>
+                </div>    
 
-            </div>
-
-        </div>
-
-    </div>   
-
-<?php endwhile; ?>    
-
-<?php endif; ?> 
-
-</div>
-
-</div>
-            </div>
-        </div>
-       
-        
     <!-- Footer Bottom area
     ================================================== -->		
     <?php get_footer(); ?>
