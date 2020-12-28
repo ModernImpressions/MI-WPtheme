77e55d98f347546b55b840c00272a3dbdbaec6df
Patrick Barnhardt <contact@crimsonstrifegames.com>
2020-12-28 09:59:22 -0500

    staff page mockup tests

--- page_staff.php
+++ page_staff.php
@@ -27,38 +27,30 @@ get_header(); ?>
                     </div>
                 </div>
                 <div class="row">
-                    <div id="staff-team-item">
-                        <?php query_posts('post_type=team_area&post_status=publish&posts_per_page=-1&paged='. get_query_var('post')); ?>
-                            <?php if(have_posts()) : ?>
-                        <?php while (have_posts()) : the_post(); ?>	
-                    <div class="staff-item">
-                        <div class="child_team_area">
+                    <div class='staff-item portrait-container'>
+                        <div class='child_team_area portrait-member'>
                             <?php the_post_thumbnail( 'team_area' ); ?>
-                            <div class="team_social">
-                                <div class="team_details">
-                                    <p>
-                                        <?php echo get_post_meta($post->ID, 'team_position', true); ?>
-                                    </p>
-                                    <?php $variable = get_post_meta($post->ID, 'team_social_list', true); ?>
+                            <ul class='team_details portrait-details'>
+                                <li></li>
+                                <li class="title"><?php echo get_post_meta($post->ID, 'team_position', true); ?></li>
+                                <li><p></p></li><BR>
+                                <li class="team_social">
+                                <?php $variable = get_post_meta($post->ID, 'team_social_list', true); ?>
                                     <?php
                                     if ( !empty( $variable ) ) {
-                                        echo '<ul>';
+                                        echo '<nav class="containersocial">';
                                         foreach( $variable as $any_name ) {
                                             echo '<li><a href="'.$any_name['team_social_link'].'"><i class="'.$any_name['team_social_icon'].'"></i></a></li>';
                                         }
-                                        echo '</ul>';
+                                        echo '</nav>';
                                     }?>
-                                </div>
-                            </div>
+                                </li>
+                            </ul>
                         </div>
-                    </div>   
-                    <?php endwhile; ?>    
-                    <?php endif; ?> 
+                    </div>
                 </div>
                 </div>
                 </div>
                 </div>    
-
-    <!-- Footer Bottom area
-    ================================================== -->		
+<!-- Footer Bottom area ================================================== -->		
     <?php get_footer(); ?>
