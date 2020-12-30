909cc86539b99199e7c41f21fbf77de6b3ad69c4
Patrick Barnhardt <contact@crimsonstrifegames.com>
2020-12-30 16:41:59 -0500

    Revert "Revert "test cta as widget""
    This reverts commit ea95ab7148438ebb731a2357f3e205d9a9c7b5d7.

--- home_part/banner_area.php
+++ home_part/banner_area.php
@@ -1,10 +1,9 @@
     <!-- Use shortcode to reference the CTA embed from Hustle. -->
-    <?php echo do_shortcode( '[wd_hustle id="3" type="embedded"/]' ); ?> <!-- this may need to be modified if the post data of the embed item changes -->
+    <!-- php echo do_shortcode '(' '[wd_hustle id="3" type="embedded"/]' ')' ; ?> --> <!-- this may need to be modified if the post data of the embed item changes -->
+    <?php dynamic_sidebar( 'sidebar-0' ); ?>
     <section id="slider">
         <div id="owl-slider-item" class="owl-carousel">
             <?php query_posts('post_type=slider&post_status=publish&posts_per_page=5&paged='. get_query_var('post')); ?>
-
-
            <?php if(have_posts()) : ?>
            <?php while (have_posts()) : the_post(); ?>	  
                 <div class="item">
