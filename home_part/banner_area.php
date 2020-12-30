476a82f0687bdf70022169ba75b6d9158f01666e
Patrick Barnhardt <contact@crimsonstrifegames.com>
2020-12-30 16:42:07 -0500

    Revert "Revert "widget zone name change test""
    This reverts commit 994c870c231b890b0a6456b379a291805b46ded6.

--- home_part/banner_area.php
+++ home_part/banner_area.php
@@ -1,6 +1,6 @@
     <!-- Use shortcode to reference the CTA embed from Hustle. -->
     <!-- php echo do_shortcode '(' '[wd_hustle id="3" type="embedded"/]' ')' ; ?> --> <!-- this may need to be modified if the post data of the embed item changes -->
-    <?php dynamic_sidebar( 'sidebar-0' ); ?>
+    <?php dynamic_sidebar( 'banner-widget' ); ?>
     <section id="slider">
         <div id="owl-slider-item" class="owl-carousel">
             <?php query_posts('post_type=slider&post_status=publish&posts_per_page=5&paged='. get_query_var('post')); ?>
