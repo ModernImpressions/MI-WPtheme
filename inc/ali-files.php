68e2f7478e435441f39e11149b2b36923d8dc7b6
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-01-07 16:44:29 -0500

    Revert "Update ali-files.php"
    This reverts commit cce374d33305c0b297384b4e18fe9841ca1a598a.

--- inc/ali-files.php
+++ inc/ali-files.php
@@ -7,7 +7,7 @@
 
 // Add all CSS Files
 function my_css_calling(){ 
-    wp_register_style('owlcarousel', get_template_directory_uri().'/libraries/owlcarousel/assets/owl.carousel.css', array(), '2.3.4', 'all' ); 
+    wp_register_style('owlcarousel', get_template_directory_uri().'/libaries/owlcarousel/assets/owl.carousel.css', array(), '2.3.4', 'all' ); 
     wp_register_style('plugins', get_template_directory_uri().'/css/plugins.css', array(), '1.0.1', 'all' ); 
     wp_enqueue_style( 'ali-style', get_stylesheet_uri() );
     wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.1', 'all' );
@@ -21,7 +21,7 @@ function my_css_calling(){
     wp_enqueue_script('jquery');
     wp_enqueue_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', 'true' );
     wp_enqueue_script('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js', array(), '4.5.0', 'true' );
-    wp_enqueue_script('owlcarousel', get_template_directory_uri() .'/libraries/owlcarousel/owl.carousel.js', array(), '2.3.4', 'true' );
+    wp_enqueue_script('owlcarousel', get_template_directory_uri() .'/libaries/owlcarousel/owl.carousel.js', array(), '2.3.4', 'true' );
     wp_enqueue_script('plugins', get_template_directory_uri() .'/js/plugins.js', array(), '1.0.1', 'true' );
     wp_enqueue_script('fontawesomepro', 'https://kit.fontawesome.com/073823df21.js', array(), '5.14.0', 'false' );
     wp_enqueue_script('main', get_template_directory_uri() .'/js/main.js', array(), '1.0.1', 'true' );
