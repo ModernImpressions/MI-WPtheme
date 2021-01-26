22bb05c41fe9320dc1b0cb7a33e2b826b3cd0b58
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-01-26 15:15:48 -0500

    testing for difficulty rating UI
    testing css

--- basepress/mikb/template-parts/article-meta-data.php
+++ basepress/mikb/template-parts/article-meta-data.php
@@ -6,20 +6,18 @@
 
 //Get ACF values for article
 $difficulty = get_field( "difficulty" ); 
-
+$deviceBrand = get_field( "device-brand" );
+$userBrand = get_field( "end-device-brand" );
 ?>
 
-
-
-
-<div class="bpress-article-metalist">
-    <span class="bpress-post-difficulty">
-        <div id="bars">
-            <div class="difftext">Difficulty: <?php echo $difficulty ?></div>
-            <div class="diffbar">
-                <div class="difflvl"></div>
+<div class="bpress-post-metalist">
+    <div id="bpress-post-access">
+        <div class="bpress-post-accessContainer">
+        <div class="bpress-post-skill-text">Difficulty: </div>
+            <div class="bpress-post-skill-tag-filter <?php echo $difficulty ?>">
+                <a id="bpress-post-skill-link" href="#"><?php echo $difficulty ?></a>
             </div>
         </div>
-    </span>
+    </div>
 </div>
 
