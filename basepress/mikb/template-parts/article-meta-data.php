54a753d20b45c502f4cfeeca4cb4b51aa2115c35
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-01-27 11:24:46 -0500

    Update article-meta-data.php

--- basepress/mikb/template-parts/article-meta-data.php
+++ basepress/mikb/template-parts/article-meta-data.php
@@ -17,7 +17,8 @@ $userBrand = get_field( "end-device-brand" );
             <div class="bpress-post-skill-tag-filter <?php echo $difficulty ?>">
                 <a id="bpress-post-skill-link" href="#"><?php echo $difficulty ?></a>
             </div>
-            <?php if ($deviceBrand != "none" or $deviceBrand != "") { ?>
+            <?php if( isset($deviceBrand) ) { ?>
+                <?php if ($deviceBrand != "none" ) { ?>
                 <div class="bpress-post-brands-text"><?php echo "Relevant Product(s): " ?></div>
                 <div class="bpress-post-brands-tag-filter <?php echo $deviceBrand ?>">
                     <a id="bpress-post-brands-link" href="#"><?php echo $deviceBrand ?></a>
@@ -25,6 +26,9 @@ $userBrand = get_field( "end-device-brand" );
             <?php } else { 
                  
             } ?>
+            <?php } else { ?>
+
+            <?php } ?>
         </div>
     </div>
 </div>
