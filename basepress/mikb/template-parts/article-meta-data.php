565e23115c655b8b4e8d664c8be088b46d444ec0
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-01-27 09:59:47 -0500

    brand display testing
    test conditionals

--- basepress/mikb/template-parts/article-meta-data.php
+++ basepress/mikb/template-parts/article-meta-data.php
@@ -13,10 +13,18 @@ $userBrand = get_field( "end-device-brand" );
 <div class="bpress-post-metalist">
     <div id="bpress-post-access">
         <div class="bpress-post-accessContainer">
-        <div class="bpress-post-skill-text">Difficulty: </div>
+            <div class="bpress-post-skill-text">Difficulty: </div>
             <div class="bpress-post-skill-tag-filter <?php echo $difficulty ?>">
                 <a id="bpress-post-skill-link" href="#"><?php echo $difficulty ?></a>
             </div>
+            <?php if ($deviceBrands != "Not Applicable") { ?>
+                <div class="bpress-post-brands-text">Relevant Products: </div>
+                <div class="bpress-post-brands-tag-filter <?php echo $deviceBrand ?>">
+                    <a id="bpress-post-brands-link" href="#"><?php echo $deviceBrand ?></a>
+                </div>
+            <?php } else { 
+                 
+            } ?>
         </div>
     </div>
 </div>
