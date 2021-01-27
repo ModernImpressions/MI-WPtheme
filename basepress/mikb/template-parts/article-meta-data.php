0c742c74d2c5bea1152512156ce0dcd561e04f45
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-01-27 15:03:02 -0500

    Update article-meta-data.php

--- basepress/mikb/template-parts/article-meta-data.php
+++ basepress/mikb/template-parts/article-meta-data.php
@@ -12,11 +12,11 @@ $relevantProducts = ($deviceBrand or $userBrand);
 ?>
 
 <div class="bpress-post-metalist">
-    <div id="bpress-post-access">
+    <div class="bpress-post-access">
         <div class="bpress-post-accessContainer">
             <div class="bpress-post-skill-text">Difficulty: </div>
             <div class="bpress-post-skill-tag-filter <?php echo $difficulty ?>">
-                <a id="bpress-post-skill-link" href="#"><?php echo $difficulty ?></a>
+                <a class="bpress-post-skill-link" href="#"><?php echo $difficulty ?></a>
             </div>
             <?php if ($relevantProducts == "true" ) { ?>
                 <div class="bpress-post-brands-text"><?php echo "Relevant Product(s): " ?></div>
@@ -24,14 +24,14 @@ $relevantProducts = ($deviceBrand or $userBrand);
             <?php if (isset($deviceBrand) ) { ?>
                 <?php if ($deviceBrand != "none" ) { ?>
                 <div class="bpress-post-brands-tag-filter <?php echo $deviceBrand ?>">
-                    <a id="bpress-post-brands-link" href="#"><?php echo $deviceBrand ?></a>
+                    <a class="bpress-post-brands-link" href="#"><?php echo $deviceBrand ?></a>
                 </div>
                 <?php } ?>
             <?php } ?>
             <?php if (isset($userBrand) ) { ?>
                 <?php if ($userBrand != "none" ) { ?>
                 <div class="bpress-post-brands-tag-filter <?php echo $userBrand ?>">
-                    <a id="bpress-post-brands-link" href="#"><?php echo $userBrand ?></a>
+                    <a class="bpress-post-brands-link" href="#"><?php echo $userBrand ?></a>
                 </div>
                 <?php } ?>
             <?php } ?>
