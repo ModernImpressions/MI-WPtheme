39fbcd68e899a1f2faa927886532a37e6cf148aa
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-01-28 11:37:11 -0500

    Update article-meta-data.php
    cleanup

--- basepress/mikb/template-parts/article-meta-data.php
+++ basepress/mikb/template-parts/article-meta-data.php
@@ -8,9 +8,6 @@
 $difficulty = get_field( "difficulty" ); 
 $deviceBrand = get_field( "device-brand" );
 $userBrand = get_field( "end-device-brand" );
-$relevantProducts = (($deviceBrand != "none") or ($userBrand != "none"));
-$deviceObject = get_field_object( "device-brand" );
-$userObject = get_field_object( "end-device-brand" );
 $deviceBrandLabel = "";
 $userBrandLabel = "";
 
@@ -65,35 +62,33 @@ switch ($userBrand) {
             <div class="bpress-post-skill-tag-filter <?php echo $difficulty ?>">
                 <a class="bpress-post-skill-link" href="#"><?php echo $difficulty ?></a>
             </div>
-            <?php if ($relevantProducts == "true" ) { ?>
-                <?php if ((isset($deviceBrand)) && (($deviceBrand != "none") or ($deviceBrand != "NULL"))) { ?>
-                    <div class="bpress-post-brands-text"><?php echo "Relevant Product(s): " ?></div>
-                <?php } elseif ((isset($userBrand)) && (($userBrand != "none") or ($userBrand != "NULL"))) { ?>
-                    <div class="bpress-post-brands-text"><?php echo "Relevant Product(s): " ?></div>
-                <?php } ?>
-            <?php } ?>
-            
             <?php if (isset($deviceBrand) ) { ?>
                 <?php if ($deviceBrand != "none" ) { ?>
-                <div class="bpress-post-brands-tag-filter <?php echo $deviceBrand ?>">
-                    <a class="bpress-post-brands-link" href="#">
-                        <?php echo $deviceBrandLabel ?>
-                    </a>
-                </div>
+                    <?php if ((isset($deviceBrand)) && (($deviceBrand != "none") or ($deviceBrand != "NULL"))) { ?>
+                    <div class="bpress-post-brands-text"><?php echo "Relevant Product: " ?></div>
+                    <?php } ?>
+                    <div class="bpress-post-brands-tag-filter <?php echo $deviceBrand ?>">
+                        <a class="bpress-post-brands-link" href="#">
+                            <?php echo $deviceBrandLabel ?>
+                        </a>
+                    </div>
                 <?php } ?>
             <?php } ?>
             <?php if (isset($userBrand) ) { ?>
                 <?php if ($userBrand != "none" ) { ?>
-                <div class="bpress-post-brands-tag-filter <?php echo $userBrand ?>">
-                    <a class="bpress-post-brands-link" href="#">
-                        <?php echo $userBrandLabel ?>
-                        <?php if ($userBrand ) { ?>
-                            <i class="fab fa-<?php echo $userBrand ?>"></i>
-                        <?php } else { ?>
-                            <i class="fas fa-<?php echo $userBrand ?>"></i>
-                        <?php } ?>
-                    </a>
-                </div>
+                    <?php if ((isset($userBrand)) && (($userBrand != "none") or ($userBrand != "NULL"))) { ?>
+                    <div class="bpress-post-brands-text"><?php echo "Affected Device: " ?></div>
+                    <?php } ?>
+                    <div class="bpress-post-brands-tag-filter <?php echo $userBrand ?>">
+                        <a class="bpress-post-brands-link" href="#">
+                            <?php echo $userBrandLabel ?>
+                            <?php if ($userBrand ) { ?>
+                                <i class="fab fa-<?php echo $userBrand ?>"></i>
+                            <?php } else { ?>
+                                <i class="fas fa-<?php echo $userBrand ?>"></i>
+                            <?php } ?>
+                        </a>
+                    </div>
                 <?php } ?>
             <?php } ?>
         </div>
