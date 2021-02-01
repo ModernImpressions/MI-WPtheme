a137b24c4c90b15afb7c45eb01b06e60f0103d53
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-02-01 15:39:14 -0500

    toc title fixes

--- basepress/mikb/functions.php
+++ basepress/mikb/functions.php
@@ -276,7 +276,7 @@ add_filter('the_content', function ($content) {
     global $tableOfContents;
 
     $tableOfContents = "
-        <div class='bpress-toc h4'>
+        <div class='bpress-toc h4 bpress-toc-title'>
             Table of Contents
         </div>
         <div class='bpress-toc-list items'>
