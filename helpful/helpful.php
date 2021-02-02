8d546601c581a57fa832ca831b7a418231aef9d0
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-02-02 11:58:06 -0500

    load helpful css in KB css

--- helpful/helpful.php
+++ helpful/helpful.php
@@ -1,7 +1,6 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
 
 <div class="helpful <?php echo esc_attr( $class ); ?>">
-
 	<?php if ( false === $hidden ) : ?>
 	<div class="helpful-header">
 		<?php echo apply_filters( 'helpful_headline_html', '<h3 class="helpful-headline">' . $helpful['heading'] . '</h3>' ); ?>
@@ -11,10 +10,8 @@
 	<div class="helpful-content" role="alert">
 		<span><?php echo $helpful['content']; ?></span>
 	</div><!-- .helpful-content -->
-
 	<?php if ( false === $hidden ) : ?>
 	<div class="helpful-controls">
-
 		<div>
 			<button class="helpful-pro helpful-button" type="button" data-value="pro" data-post="<?php echo $helpful['post_id']; ?>" role="button">
 				<?php echo $helpful['button_pro']; ?>
@@ -31,14 +28,4 @@
 
 	</div><!-- .helpful-controls -->
 	<?php endif; ?>
-
-	<?php if ( 'on' === $helpful['credits'] && false === $hidden ) : ?>
-	<div class="helpful-footer">
-		<?php
-		/* translators: %s = credits link */
-		printf( _x( 'Powered by %s', 'credits', 'helpful' ), $helpful['credits_html'] );
-		?>
-	</div><!-- .helpful-footer -->
-	<?php endif; ?>
-
 </div><!-- .helpful -->
