460b7c9ae47dd1eccbf0085e22f5c2d068f83637
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-03-23 08:49:56 -0400

    base setup for testimonial tests
    setup requirements to pull in the CEOjuice query

---
+++ home_part/testimonials_area.php
@@ -0,0 +1,7 @@
+<section id="testimonial_area">
+            <div class="container">
+                <div class="row">
+                     <?php if(file_exists('other_part\ceojuice-commentsquery\testimonials_part.php')) include_once 'other_part\ceojuice-commentsquery\testimonials_part.php'; ?>
+                </div>
+            </div>
+        </section>
