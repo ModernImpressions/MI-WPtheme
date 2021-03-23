8992698153f1f35e0a90b50d6ecc55463183b8d8
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-03-23 15:11:57 -0400

    Update testimonials_area.php

--- home_part/testimonials_area.php
+++ home_part/testimonials_area.php
@@ -1,7 +1,9 @@
 <section id="testimonial_area">
-            <div class="container">
-                <div class="row">
-                     <?php if(file_exists('other_part\ceojuice-commentsquery\testimonials_part.php')) include_once 'other_part\ceojuice-commentsquery\testimonials_part.php'; ?>
-                </div>
-            </div>
-        </section>
+    <div class="container">
+        <div class="row">
+            <?php if (file_exists('other_part\ceojuice-commentsquery\testimonials_part.php')) {
+                include 'other_part\ceojuice-commentsquery\testimonials_part.php'; 
+            } ?>
+        </div>
+    </div>
+</section>
