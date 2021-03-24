f22cd05461ed9762260869fa174f39c9ac613450
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-03-24 11:20:15 -0400

    Update testimonials_area.php

--- home_part/testimonials_area.php
+++ home_part/testimonials_area.php
@@ -5,23 +5,12 @@
 		        $('#customers-testimonials').owlCarousel({
 		            loop: true,
 		            center: true,
-		            items: 3,
+		            items: 1,
 		            margin: 0,
 		            autoplay: true,
 		            dots:true,
 		            autoplayTimeout: 8500,
 		            smartSpeed: 450,
-		            responsive: {
-		              0: {
-		                items: 1
-		              },
-		              768: {
-		                items: 2
-		              },
-		              1170: {
-		                items: 3
-		              }
-		            }
 		        });
         	});
 </script>
