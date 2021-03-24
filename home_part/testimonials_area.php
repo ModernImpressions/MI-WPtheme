2e380d2143e372d6469007119066019f06e1ef2e
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-03-24 10:48:31 -0400

    testimonials slider tests

--- home_part/testimonials_area.php
+++ home_part/testimonials_area.php
@@ -1,8 +1,35 @@
-<section id="testimonial_area">
+<script>
+    jQuery(document).ready(function($) {
+        		"use strict";
+        		//  TESTIMONIALS CAROUSEL HOOK
+		        $('#customers-testimonials').owlCarousel({
+		            loop: true,
+		            center: true,
+		            items: 3,
+		            margin: 0,
+		            autoplay: true,
+		            dots:true,
+		            autoplayTimeout: 8500,
+		            smartSpeed: 450,
+		            responsive: {
+		              0: {
+		                items: 1
+		              },
+		              768: {
+		                items: 2
+		              },
+		              1170: {
+		                items: 3
+		              }
+		            }
+		        });
+        	});
+</script>
+<section id="testimonial_area" class="testimonials">
     <div class="container">
         <div class="row">
-            <div class="testimonial-container">
-                <div class="owl-carousel owl-theme">
+            <div class="col-sm-12">
+                <div id="customers-testimonials" class="owl-carousel">
                     <?php get_template_part('other_part/ceojuice-commentsquery/testimonials_part'); ?>
                 </div>
             </div>
