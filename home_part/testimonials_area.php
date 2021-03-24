32390b22c69f3eff34aa9839569273816a237920
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-03-24 10:33:25 -0400

    Update testimonials_area.php

--- home_part/testimonials_area.php
+++ home_part/testimonials_area.php
@@ -1,15 +1,10 @@
 <section id="testimonial_area">
-    <div>
-        <div>
+    <div class="container">
+        <div class="row">
             <div class="testimonial-container">
-                <input type="radio" name="nav" id="first" checked/>
-                <input type="radio" name="nav" id="second" />
-                <input type="radio" name="nav" id="third" />
-  
-                <label for="first" class="first"></label>
-                <label for="second"  class="second"></label>
-                <label for="third" class="third"></label>
-                <?php get_template_part('other_part/ceojuice-commentsquery/testimonials_part'); ?>
+                <div class="owl-carousel owl-theme">
+                    <?php get_template_part('other_part/ceojuice-commentsquery/testimonials_part'); ?>
+                </div>
             </div>
         </div>
     </div>
