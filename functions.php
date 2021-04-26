eb4d11bbd944cf4b1d79667dc22a346c52780f70
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-04-26 15:48:59 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -206,7 +206,7 @@ function woo_product_subcategories( $args = array() ) {
 		echo '<ul class="product-cats">';
 	 
 		foreach ( $terms as $term ) {
-			echo '<li class="category">';
+			echo '<li class="category cat-' . $term->slug . '">';
 			woocommerce_subcategory_thumbnail( $term );
 				echo '<h2>';
 					echo '<a href="' .  esc_url( get_term_link( $term ) ) . '" class="' . $term->slug . '">';
