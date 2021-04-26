2e5a4c7bc39b75d5b3e995a34d10b863cf00f419
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-04-26 13:14:36 -0400

    Update functions.php
    setup function to display categories on the shop page

--- functions.php
+++ functions.php
@@ -181,10 +181,43 @@ function register_helpful_shortcode( $atts ) {
  */
 add_shortcode( 'helpful_pro', 'register_helpful_shortcode' );
 
+/** 
+ * Declare WooCommerce Support
+ */
 function add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', 'add_woocommerce_support' );
+
+/** 
+ * Setup Custom Display of Categories on the Shop/Catalog page.
+ */
+function woo_product_subcategories( $args = array() ) {
+	$parentid = get_queried_object_id();
+ 
+	$args = array(
+		'parent' => $parentid
+	);
+	 
+	$terms = get_terms( 'product_cat', $args );
+	 
+	if ( $terms ) {
+		echo '<ul class="product-cats">';
+	 
+		foreach ( $terms as $term ) {
+			echo '<li class="category">';
+				echo '<h2>';
+					echo '<a href="' .  esc_url( get_term_link( $term ) ) . '" class="' . $term->slug . '">';
+						echo $term->name;
+					echo '</a>';
+				echo '</h2>';
+			echo '</li>';
+		}
+		echo '</ul>';
+	}
+}
+ 
+add_action( 'woocommerce_before_shop_loop', 'woo_product_subcategories', 50 );
 
 /**
  * Allows the use of shortcuts in widgets.
