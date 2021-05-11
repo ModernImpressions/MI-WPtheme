8072a18f07e1da61198e5624a2a9d0d639fc76a3
Patrick Barnhardt <contact@crimsonstrifegames.com>
2021-05-11 13:56:56 -0400

    test sidebar location

--- woocommerce/archive-category.php
+++ woocommerce/archive-category.php
@@ -140,6 +140,11 @@ do_action( 'woocommerce_before_main_content' );
 							}
 							?>
                         </div>
+						<div class="col-md-2">
+						<div class="sideber_main_area">
+							<?php dynamic_sidebar( 'filter-widget' ); ?>
+						</div>
+					</div>
 						<?php 
 						/**
  						* Hook: woocommerce_after_main_content.
@@ -149,11 +154,6 @@ do_action( 'woocommerce_before_main_content' );
 						do_action( 'woocommerce_after_main_content' );
 						?>
                     </div>
-					<div class="col-md-5">
-						<div class="sideber_main_area">
-							<?php dynamic_sidebar( 'filter-widget' ); ?>
-						</div>
-					</div>
                 </div>
             </div>
         </div>  
