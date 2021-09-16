f428ec2defd6466b9d0d2d70c92796c52b826359
Patrick Barnhardt <contact@patrickbarnhardt.info>
2021-09-16 15:46:57 -0400

    fix for dumb centering code from Triad. also embed tests for homepage.

--- home_part/impression_area.php
+++ home_part/impression_area.php
@@ -1,30 +1,14 @@
         <section id="impression_area">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-1"></div>
                     <div class="col-md-10">
                         <?php if (function_exists('get_option_tree')) : if (get_option_tree('impression_textarea')) : ?>
                         <?php get_option_tree('impression_textarea', '', 'true'); ?>
-                        <style>
-                        .ytvideo-embed {
-                            position: relative;
-                            width: 100%;
-                            height: 0;
-                            padding-bottom: 56.27198%;
-                        }
-
-                        .ytvideo-embed iframe {
-                            position: absolute;
-                            top: 0;
-                            left: 0;
-                            width: 100%;
-                            height: 100%;
-                        }
-
-                        </style>
-                        <div class="ytvideo-embed"><iframe width='420' height='247'
-                                src="https://www.youtube.com/embed/VXYd89PQe7w?&theme=light&autohide=2&modestbranding=1&rel=0"
-                                frameborder="0"></iframe></div>
+                        <div class="su-youtube su-u-responsive-media-yes"><iframe allowfullscreen=""
+                                allow="autoplay; encrypted-media; picture-in-picture"
+                                title="Introduction to Modern Impressions"
+                                data-src="https://www.youtube.com/watch?v=USgOhNJGxjs?autohide=1&amp;autoplay=0&amp;mute=0&amp;controls=1&amp;fs=1&amp;loop=1&amp;modestbranding=1&amp;playlist=VXYd89PQe7w&amp;rel=1&amp;showinfo=1&amp;theme=light&amp;wmode=&amp;playsinline=1"
+                                class=" lazyloaded" width="600" height="400" frameborder="0"></iframe></div>
                         <?php else : ?>
                         <?php endif;
                         endif; ?>
