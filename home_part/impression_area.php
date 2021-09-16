3d83cdd522ac10de053aa2a2980c340e558bbdff
Patrick Barnhardt <contact@patrickbarnhardt.info>
2021-09-16 15:33:46 -0400

    Update impression_area.php

--- home_part/impression_area.php
+++ home_part/impression_area.php
@@ -3,29 +3,50 @@
                 <div class="row">
                     <div class="col-md-1"></div>
                     <div class="col-md-10">
-                        <?php if ( function_exists( 'get_option_tree') ) : if( get_option_tree( 'impression_textarea') ) : ?>
-                            <?php get_option_tree( 'impression_textarea', '', 'true' ); ?>
+                        <?php if (function_exists('get_option_tree')) : if (get_option_tree('impression_textarea')) : ?>
+                        <?php get_option_tree('impression_textarea', '', 'true'); ?>
+                        <style>
+                        .ytvideo-embed {
+                            position: relative;
+                            width: 100%;
+                            height: 0;
+                            padding-bottom: 56.27198%;
+                        }
+
+                        .ytvideo-embed iframe {
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                            width: 100%;
+                            height: 100%;
+                        }
+
+                        </style>
+                        <div class="ytvideo-embed"><iframe width='420' height='247'
+                                src="https://www.youtube.com/embed/VXYd89PQe7w?&theme=light&autohide=2&modestbranding=1&rel=0"
+                                frameborder="0"></iframe></div>
                         <?php else : ?>
-                        <?php endif; endif; ?> 
+                        <?php endif;
+                        endif; ?>
                     </div>
                 </div>
-                
+
                 <?php
-                    $list_item = ot_get_option( 'impression_list', array() );
-                    if ( !empty( $list_item ) ) {
-                            echo '<div class="row">';
-                        foreach( $list_item as $type ) {
-                            echo '
+                $list_item = ot_get_option('impression_list', array());
+                if (!empty($list_item)) {
+                    echo '<div class="row">';
+                    foreach ($list_item as $type) {
+                        echo '
                             <div class="col-md-3">
                                 <div class="child_impression">
-                                    <h2>'.$type['impression_number'].'</h2>
-                                    <h5>'.$type['impression_title'].'</h5>
-                                    <p>'.$type['impression_textarea'].'</p>
+                                    <h2>' . $type['impression_number'] . '</h2>
+                                    <h5>' . $type['impression_title'] . '</h5>
+                                    <p>' . $type['impression_textarea'] . '</p>
                                 </div>
                             </div>';
-                        }
-                            echo '</div>';
                     }
-                ?> 
+                    echo '</div>';
+                }
+                ?>
             </div>
         </section>
