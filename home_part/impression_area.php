70879f6de137452388deaad9f6bbd8158b7b282f
Patrick Barnhardt <contact@patrickbarnhardt.info>
2021-09-20 15:35:19 -0400

    Update impression_area.php

--- home_part/impression_area.php
+++ home_part/impression_area.php
@@ -4,6 +4,7 @@
                     <div class="col-md-10">
                         <?php if (function_exists('get_option_tree')) : if (get_option_tree('impression_textarea')) : ?>
                         <?php get_option_tree('impression_textarea', '', 'true'); ?>
+                        <br />
                         <?php echo do_shortcode('[su_youtube_advanced url="https://www.youtube.com/watch?v=USgOhNJGxjs" loop="1" controls="alt" autohide="yes" theme="light" modestbranding="yes" wmode="transparent" playsinline="yes" title="An Introduction to Modern Impressions"]'); ?>
                         <?php else : ?>
                         <?php endif;
