0c6edb19592514a87b11fc2c2ae028f0449b68b5
Patrick Barnhardt <contact@patrickbarnhardt.info>
2021-09-30 10:14:40 -0400

    Update two-columns-left.php

--- basepress/mikb/two-columns-left.php
+++ basepress/mikb/two-columns-left.php
@@ -5,83 +5,85 @@
 $bpkb_knowledge_base = basepress_kb();
 $bpkb_is_single_kb = basepress_is_single_kb();
 
-basepress_get_header( 'basepress' );
+basepress_get_header('basepress');
 ?>
 
-	<!-- Main BasePress wrap -->
-	<div class="bpress-wrap">
+<!-- Main BasePress wrap -->
+<div class="bpress-wrap">
 
-	<div class="bpress-page-header">
-		<div class="bpress-content-wrap">
-			<!-- Knowledge Base title -->
-			<header>
-				<h2><?php echo apply_filters( 'basepress_modern_theme_header_title', 'Knowledge Base' ); ?><br><?php echo ( $bpkb_is_single_kb ? '' : $bpkb_knowledge_base->name ); ?></h2>
-			</header>
+    <div class="bpress-page-header">
+        <div class="bpress-content-wrap">
+            <!-- Knowledge Base title -->
+            <header>
+                <h2><?php echo apply_filters('basepress_modern_theme_header_title', 'Knowledge Base'); ?><br><?php echo ($bpkb_is_single_kb ? '' : $bpkb_knowledge_base->name); ?>
+                </h2>
+            </header>
 
-			<!-- Add searchbar -->
-			<div class="bpress-searchbar-wrap">
-				<?php basepress_searchbar(); ?>
-			</div>
-		</div>
-	</div>
+            <!-- Add searchbar -->
+            <div class="bpress-searchbar-wrap">
+                <?php basepress_searchbar(); ?>
+            </div>
+        </div>
+    </div>
 
-	<!-- Add breadcrumbs -->
-	<div class="bpress-crumbs-wrap">
-		<div class="bpress-content-wrap">
-			<?php basepress_breadcrumbs(); ?>
-		</div>
-	</div>
+    <!-- Add breadcrumbs -->
+    <div class="bpress-crumbs-wrap">
+        <div class="bpress-content-wrap">
+            <?php basepress_breadcrumbs(); ?>
+        </div>
+    </div>
 
-	<div class="bpress-content-wrap">
-		<div class="bpress-content-area bpress-float-right">
+    <div class="bpress-content-wrap">
+        <div class="bpress-content-area bpress-float-right">
 
-			<!-- Add main content -->
-			<main class="bpress-main" role="main">
+            <!-- Add main content -->
+            <main class="bpress-main" role="main">
 
-				<?php
+                <?php
 				//Start the loop.
-				while ( have_posts() ) : the_post();
+				while (have_posts()) : the_post();
 
 					//Include the page content template using basepress internal function.
-					basepress_get_template_part( 'post-content' );
+					basepress_get_template_part('post-content');
 
 					//Get Polls Items
 					basepress_votes();
 
-					//End of the loop.
+				//End of the loop.
 				endwhile;
 
 				?>
 
-				<!-- Add previous and next articles navigation -->
-				<?php basepress_get_template_part( 'adjacent-articles' ); ?>
+                <!-- Add previous and next articles navigation -->
+                <?php basepress_get_template_part('adjacent-articles'); ?>
 
-			</main>
+            </main>
 
-			<?php
+            <?php
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
+			if (comments_open() || get_comments_number()) {
 				comments_template();
 			}
 			?>
 
-			</div><!-- content area -->
+        </div><!-- content area -->
 
-			<!-- Sidebar -->
-			<?php if ( is_active_sidebar( 'basepress-sidebar' ) ) : ?>
-				<aside class="bpress-sidebar bpress-float-right" role="complementary">
-					<div class="hide-scrollbars">
-						<?php dynamic_sidebar( 'basepress-sidebar' ); ?>
-					</div>
-					<br/>
-					<div class="hide-scrollbars">
-						<h3 class="widget-title">Need Support?</h3>
-						<p>Can't find the answer you're looking for? Don't worry we're here to help!</p>
-						<a class="button primary-button large full" href="#">contact support</a>
-					</div>
-				</aside>
-			<?php endif; ?>
-		</div>
-	</div><!-- .wrap -->
+        <!-- Sidebar -->
+        <?php if (is_active_sidebar('basepress-sidebar')) : ?>
+        <aside class="bpress-sidebar bpress-float-right" role="complementary">
+            <div class="hide-scrollbars">
+                <?php dynamic_sidebar('basepress-sidebar'); ?>
+            </div>
+            <br />
+            <div class="hide-scrollbars">
+                <h3 class="widget-title">Need Support?</h3>
+                <p>Can't find the answer you're looking for? Don't worry we're here to help!</p>
+                <a class="button primary-button large full bpress-search-submit"
+                    href="<?php echo site_url('/support/place-a-service-call'); ?>">Contact Support</a>
+            </div>
+        </aside>
+        <?php endif; ?>
+    </div>
+</div><!-- .wrap -->
 
-<?php basepress_get_footer( 'basepress' ); ?>
+<?php basepress_get_footer('basepress'); ?>
