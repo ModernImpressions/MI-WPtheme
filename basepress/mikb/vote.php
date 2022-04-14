ff988052af9a6b348b47523ecc26a23e9dd6b2d1
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-04-14 12:24:09 -0400

    Update vote.php

--- basepress/mikb/vote.php
+++ basepress/mikb/vote.php
@@ -2,48 +2,48 @@
 
 function voteme_enqueuescripts()
 {
-	wp_enqueue_script('voteme', get_template_directory_uri() .'/basepress/mikb/js/bpkb_vote.js', array('jquery'));
-	wp_localize_script( 'voteme', 'votemeajax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+	wp_enqueue_script('voteme', get_template_directory_uri() . '/basepress/mikb/js/bpkb_vote.js', array('jquery'));
+	wp_localize_script('voteme', 'votemeajax', array('ajaxurl' => admin_url('admin-ajax.php')));
 }
 add_action('wp_enqueue_scripts', voteme_enqueuescripts);
 
 function voteme_getvotelink()
 {
-$votemelink = "";
+	$votemelink = "";
 
-$post_ID = get_the_ID();
-$votemecount = get_post_meta($post_ID, '_votemecount', true) != '' ? get_post_meta($post_ID, '_votemecount', true) : '0';
+	$post_ID = get_the_ID();
+	$votemecount = get_post_meta($post_ID, '_votemecount', true) != '' ? get_post_meta($post_ID, '_votemecount', true) : '0';
 
-$link = $votemecount.' <a onclick="votemeaddvote('.$post_ID.');">'.'Vote'.'</a>';
+	$link = ' <a onclick="votemeaddvote(' . $post_ID . ');">' . 'Vote' . '</a>';
 
-$votemelink = '<div id="voteme-'.$post_ID.'">';
-$votemelink .= '<span>'.$link.'</span>';
-$votemelink .= '</div>';
+	$votemelink = '<div id="voteme-' . $post_ID . '">';
+	$votemelink .= '<span>' . $link . '</span>';
+	$votemelink .= '</div>';
 
-return $votemelink;
+	return $votemelink;
 }
 
 function voteme_printvotelink($content)
 {
-return $content.voteme_getvotelink();
+	return $content . voteme_getvotelink();
 }
 add_filter('the_content', voteme_printvotelink);
 
 function voteme_addvote()
 {
-		$results = '';
-		global $wpdb;
-		$post_ID = $_POST['postid'];
-		$votemecount = get_post_meta($post_ID, '_votemecount', true) != '' ? get_post_meta($post_ID, '_votemecount', true) : '0';
-		$votemecountNew = $votemecount + 1;
-		update_post_meta($post_ID, '_votemecount', $votemecountNew);
+	$results = '';
+	global $wpdb;
+	$post_ID = $_POST['postid'];
+	$votemecount = get_post_meta($post_ID, '_votemecount', true) != '' ? get_post_meta($post_ID, '_votemecount', true) : '0';
+	$votemecountNew = $votemecount + 1;
+	update_post_meta($post_ID, '_votemecount', $votemecountNew);
 
-		$results.='<div class="votescore" >'.$votemecountNew.'</div>';
+	$results .= ''; // removed <div class="votescore" >'.$votemecountNew.'</div> from the string to prevent the count from showing on the pages.
 
-		// Return the String
-		die($results);
-	}
+	// Return the String
+	die($results);
+}
 
-		// creating Ajax call for WordPress
-		add_action( 'wp_ajax_nopriv_voteme_addvote', 'voteme_addvote' );
-		add_action( 'wp_ajax_voteme_addvote', 'voteme_addvote' );
+// creating Ajax call for WordPress
+add_action('wp_ajax_nopriv_voteme_addvote', 'voteme_addvote');
+add_action('wp_ajax_voteme_addvote', 'voteme_addvote');
