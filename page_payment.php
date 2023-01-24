78578149ae906b8c6794c8fea8982b49b4456a4f
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-01-23 19:28:40 -0500

    Update page_payment.php

--- page_payment.php
+++ page_payment.php
@@ -23,71 +23,7 @@ $merchantTransactionKey = get_option('MERCHANT_TRANSACTION_KEY');
         <div class="row">
             <div class="col-md-8">
                 <div class="original_content_area">
-                    <?php
-                    function getAnAcceptPaymentPage()
-                    {
-                        /* Create a merchantAuthenticationType object with authentication details
-       retrieved from the constants file */
-                        $merchantAuthentication = new AnetAPI\MerchantAuthenticationType();
-                        $merchantAuthentication->setName(get_option('MERCHANT_LOGIN_ID'));
-                        $merchantAuthentication->setTransactionKey(get_option('MERCHANT_TRANSACTION_KEY'));
-
-                        // Set the transaction's refId
-                        $refId = 'ref' . time();
-
-                        //create a transaction
-                        $transactionRequestType = new AnetAPI\TransactionRequestType();
-                        $transactionRequestType->setTransactionType("authCaptureTransaction");
-                        $transactionRequestType->setAmount("12.23");
-
-                        // Set Hosted Form options
-                        $setting1 = new AnetAPI\SettingType();
-                        $setting1->setSettingName("hostedPaymentButtonOptions");
-                        $setting1->setSettingValue("{\"text\": \"Pay\"}");
-
-                        $setting2 = new AnetAPI\SettingType();
-                        $setting2->setSettingName("hostedPaymentOrderOptions");
-                        $setting2->setSettingValue("{\"show\": false}");
-
-                        $setting3 = new AnetAPI\SettingType();
-                        $setting3->setSettingName("hostedPaymentReturnOptions");
-                        $setting3->setSettingValue(
-                            "{\"url\": \"https://modernimpressions.com/receipt\", \"cancelUrl\": \"https://modernimpressions.com/cancel\", \"showReceipt\": true}"
-                        );
-
-                        // Build transaction request
-                        $request = new AnetAPI\GetHostedPaymentPageRequest();
-                        $request->setMerchantAuthentication($merchantAuthentication);
-                        $request->setRefId($refId);
-                        $request->setTransactionRequest($transactionRequestType);
-
-                        $request->addToHostedPaymentSettings($setting1);
-                        $request->addToHostedPaymentSettings($setting2);
-                        $request->addToHostedPaymentSettings($setting3);
-
-                        //execute request
-                        $controller = new AnetController\GetHostedPaymentPageController($request);
-                        $response = $controller->executeWithApiResponse(\net\authorize\api\constants\ANetEnvironment::SANDBOX);
-
-                        if (($response != null) && ($response->getMessages()->getResultCode() == "Ok")) {
-                            //echo $response->getToken() . "\n";
-                        } else {
-                            echo "ERROR :  Failed to get hosted payment page token\n";
-                            $errorMessages = $response->getMessages()->getMessage();
-                            echo "RESPONSE : " . $errorMessages[0]->getCode() . "  " . $errorMessages[0]->getText() . "\n";
-                        }
-                        return $response;
-                    }
-                    if (!defined('DONT_RUN_SAMPLES')) {
-                        getAnAcceptPaymentPage();
-                        if ($response != null) {
-                            // Send an HTML POST to the hosted payment page
-                            $token = $response->getToken();
-                            $url = "https://accept.authorize.net/payment/payment";
-                            $url .= "?token=" . $token;
-                            echo "<script type='text/javascript'>window.location.href = '" . $url . "';</script>"; // Redirect to hosted payment page
-                        }
-                    } ?>
+                    <?php ?>
                 </div>
             </div>
             <div class="col-md-4">
