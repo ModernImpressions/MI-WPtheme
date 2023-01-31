9d636e74914fdf9eb51f35286d37f057b7925c9a
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-01-31 16:34:30 -0500

    Update page_payment.php

--- page_payment.php
+++ page_payment.php
@@ -20,14 +20,18 @@ $paymentURL = 'https://test.authorize.net/payment/payment' // Default to test en
         <div class="row">
             <div class="col-md-8">
                 <div class="original_content_area">
-                    <?php $paymentToken = getAnAcceptPaymentPage($merchantLoginID, $merchantTransactionKey, 12.50, $merchantEnv); ?>
                     <?php /** Set URL based on environment */ if ($merchantEnv == "SANDBOX") {
                         $paymentURL = 'https://test.authorize.net/payment/payment';
                     } elseif ($merchantEnv == "PRODUCTION") {
                         $paymentURL = 'https://accept.authorize.net/payment/payment';
                     } ?>
+                    <!-- Form to capture information to send to Authorize.net -->
+                    <form>
+
+                    </form>
                     <form method="post" action="<?php echo $paymentURL; ?>" id="formAuthorizeNetTestPage"
                         name="formAuthorizeNetTestPage">
+                        <?php $paymentToken = getAnAcceptPaymentPage($merchantLoginID, $merchantTransactionKey, 12.50, $merchantEnv); ?>
                         <input type="hidden" name="token" value="<?php echo $paymentToken; ?>" />Redirect- Continue to
                         Authorize.net to Payment Page
                         <button id="btnContinue">Continue to next page</button>
