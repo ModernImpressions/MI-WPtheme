2707a6bf758d5d31bb063e35ce9191ef9a2ca8fb
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-01-31 13:03:06 -0500

    Update page_payment.php

--- page_payment.php
+++ page_payment.php
@@ -14,6 +14,7 @@ define("AUTHORIZENET_LOG_FILE", "phplog");
 //merchant credentials
 $merchantLoginID = get_option('MERCHANT_LOGIN_ID');
 $merchantTransactionKey = get_option('MERCHANT_TRANSACTION_KEY');
+$merchantEnv = get_option('aNetENV');
 ?>
 
 <!-- Content Area
@@ -23,7 +24,13 @@ $merchantTransactionKey = get_option('MERCHANT_TRANSACTION_KEY');
         <div class="row">
             <div class="col-md-8">
                 <div class="original_content_area">
-                    <?php ?>
+                    <?php $paymentToken = getAnAcceptPaymentPage($merchantLoginID, $merchantTransactionKey, 12.50, $merchantEnv); ?>
+                    <form method="post" action="https://test.authorize.net/payment/payment"
+                        id="formAuthorizeNetTestPage" name="formAuthorizeNetTestPage">
+                        <input type="hidden" name="token" value="<?php $paymentToken; ?>" />Redirect- Continue to
+                        Authorize.net to Payment Page
+                        <button id="btnContinue">Continue to next page</button>
+                    </form>
                 </div>
             </div>
             <div class="col-md-4">
