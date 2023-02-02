86d8604147706d1930f37ba87de72376ed3abbd4
Patrick Barnhardt <contact@patrickbarnhardt.info>
2023-02-02 11:08:49 -0500

    Update page_payment.php

--- page_payment.php
+++ page_payment.php
@@ -22,7 +22,19 @@ $paymentURL = 'https://test.authorize.net/payment/payment' // Default to test en
     <div class="container">
         <div class="row">
             <div class="col-md-7">
-                <div class="original_content_area">
+                < class="original_content_area">
+                    <h1>Online Payment Form</h1>
+                    <br />
+                    <p>Welcome to our Online Payments portal. Please provide the invoice information below, you will
+                        be redirected to Authorize.net to complete the payment. Modern Impressions does not collect
+                        payment information on our site.</p>
+                    <br />
+                    <p>You can pay online at <a href="<?php echo get_site_url() ?>">modernimpressions.com</a>
+                        with confidence. We have partnered with <a href="https://www.authorize.net">Authorize.Net</a>, a
+                        leading payment gateway since
+                        1996, to accept credit cards and electronic check payments safely and securely for our
+                        customers.</p>
+                    <hr />
                     <?php /** Set URL based on environment */ if ($merchantEnv == "SANDBOX") {
                         $paymentURL = 'https://test.authorize.net/payment/payment';
                     } elseif ($merchantEnv == "PRODUCTION") {
@@ -33,7 +45,7 @@ $paymentURL = 'https://test.authorize.net/payment/payment' // Default to test en
                     //Check if the invoiceAmount and invoiceNumber are set in $_POST
                     if (!isset($_POST['invoiceNumber'], $_POST['invoiceAmount'])) { ?>
                     <form method="post" action="">
-                        <h2>Invoice Retrieval Form</h2>
+                        <h3>Invoice Retrieval Form</h3>
                         <p>Enter the invoice number you wish to pay.</p>
                         <label for="invoiceNumber">Invoice Number*</label>
                         <input type="text" name="invoiceNumber" placeholder=" " required />
@@ -56,52 +68,74 @@ $paymentURL = 'https://test.authorize.net/payment/payment' // Default to test en
                         <button id="btnContinue">Continue to next page</button>
                     </form>
                     <?php } ?>
-                </div>
-            </div>
-            <div class="col-md-5">
-                <section class="container">
-                    <div class="row is-flex row-space">
-                        <div class="col-md-2 col-xs-12 small-none is-center is-right">
-                            <figure class="icon-for-section credit">
-                            </figure>
-                        </div>
-                        <div class="col-md-10 col-xs-8">
-                            <h5 class="typo-h5">Credit & debit cards</h5>
-                            <p>We accept Visa, Mastercard, American Express, Discover, JCB, and Diners Club.
-                            </p>
-                            <ul>
-                                <li class="card-brands visa"></li>
-                                <li class="card-brands marter-card"></li>
-                                <li class="card-brands american-express"></li>
-                                <li class="card-brands discover"></li>
-                                <li class="card-brands jcb"></li>
-                                <li class="card-brands diners-club"></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row is-flex row-space">
-                        <div class="col-md-2 col-xs-12 small-none is-center is-right">
-                            <div>
-                                <figure class="icon-for-section ach"></figure>
-                            </div>
-                        </div>
-                        <div class="col-md-10 col-xs-8">
-                            <h5 class="typo-h5">ACH payments</h5>
-                            <p>ACH payments are accepted.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="row is-flex row-space">
-                        <h5 class="typo-h5">Contact Us</h5>
-                        <p>For any questions, please contact us at <a
-                                href="mailto:careteam@modernimpressions.com">careteam@modernimpressions.com</a>
-                            or
-                            call us at <a href="tel:1-704-597-7278">1-704-597-7278</a></p>
-                    </div>
-                </section>
+                    <hr />
+                    <p>The Authorize.Net Payment Gateway manages the complex routing of sensitive customer information
+                        through the electronic check and credit card processing networks. See an <a
+                            href="https://www.authorize.net/resources/howitworksdiagram/">online payments diagram</a> to
+                        see how it works.</p>
+                    <p>The company adheres to strict industry standards for payment processing, including:
+                        <br />
+                    <ul>
+                        <li>128-bit Secure Sockets Layer (SSL) technology for secure Internet Protocol (IP)
+                            transactions.</li>
+                        <li>Industry leading encryption hardware and software methods and security protocols to protect
+                            customer information.</li>
+                        <li>Compliance with the Payment Card Industry Data Security Standard (PCI DSS).</li>
+                    </ul>
+                    <br />
+                    For additional information regarding the privacy of your sensitive cardholder data, please read the
+                    <a href="https://www.authorize.net/company/privacy/">Authorize.Net Privacy Policy</a>.
+                    </p>
+                    <p><a href="<?php echo get_site_url() ?>">modernimpressions.com</a> is registered with the
+                        Authorize.Net Verified Merchant Seal program.</p>
+                    <!-- Authorize.Net Seal -->
+                    <div>PLACEHOLDER FOR SEAL CODE</div>
             </div>
         </div>
+        <div class="col-md-5">
+            <section class="container">
+                <div class="row is-flex row-space">
+                    <div class="col-md-2 col-xs-12 small-none is-center is-right">
+                        <figure class="icon-for-section credit">
+                        </figure>
+                    </div>
+                    <div class="col-md-10 col-xs-8">
+                        <h5 class="typo-h5">Credit & debit cards</h5>
+                        <p>We accept Visa, Mastercard, American Express, Discover, JCB, and Diners Club.
+                        </p>
+                        <ul>
+                            <li class="card-brands visa"></li>
+                            <li class="card-brands marter-card"></li>
+                            <li class="card-brands american-express"></li>
+                            <li class="card-brands discover"></li>
+                            <li class="card-brands jcb"></li>
+                            <li class="card-brands diners-club"></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row is-flex row-space">
+                    <div class="col-md-2 col-xs-12 small-none is-center is-right">
+                        <div>
+                            <figure class="icon-for-section ach"></figure>
+                        </div>
+                    </div>
+                    <div class="col-md-10 col-xs-8">
+                        <h5 class="typo-h5">ACH payments</h5>
+                        <p>ACH payments are accepted.
+                        </p>
+                    </div>
+                </div>
+                <div class="row is-flex row-space">
+                    <h5 class="typo-h5">Contact Us</h5>
+                    <p>For any questions, please contact us at <a
+                            href="mailto:careteam@modernimpressions.com">careteam@modernimpressions.com</a>
+                        or
+                        call us at <a href="tel:1-704-597-7278">1-704-597-7278</a></p>
+                </div>
+            </section>
+        </div>
     </div>
+</div>
 </div>
 
 
